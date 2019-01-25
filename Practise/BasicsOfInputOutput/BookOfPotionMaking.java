c56245233f53aa70da441eba3d86ab2e1bbddee1
MEPHISTOPHELSES <roysumit949@gmail.com>
2019-01-25 10:45:17 +0530

    All test cases passed

---
+++ Practise/BasicsOfInputOutput/BookOfPotionMaking.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BookOfPotionMaking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int isbn = in.nextInt();
+		int[] num = new int[10];
+		String str = Integer.toString(isbn);
+		int flag =0;
+		if(str.length() < 10)
+		{
+			flag =1;
+		}
+		else
+		{
+			for(int i=9; i>=0; i++)
+			{
+				num[i] =  
+			}
+			
+		}
+
+	}
+
+}
